68ffb962af378d468d7653adaf14dc7db2d7d8a0
AndersonAD <alvesduarte97@gmail.com>
2022-08-25 16:51:44 -0300

    First

---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+import "./Button.css";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => {
+    return <button className='button'> {props.label} </button>;
+}
